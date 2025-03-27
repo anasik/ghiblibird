8f8b79f67cccb3c925f832b525ba7ad57d26286a
Anas Ismail Khan <anasismail28@gmail.com>
2025-03-27 20:56:49 +0500

    favicon and somewhat responsive

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ window.onload = function() {
   // Canvas setup
   const canvas = document.getElementById("gameCanvas");
   const ctx = canvas.getContext("2d");
-
+  
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+  resizeCanvas();
+  window.addEventListener("resize", () => {
+    // For simplicity, restart the game on resize.
+    resetGame();
+    resizeCanvas();
+  });
+  
   const canvasWidth = canvas.width;
   const canvasHeight = canvas.height;
   let frames = 0;
